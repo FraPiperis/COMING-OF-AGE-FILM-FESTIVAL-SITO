cd39e918f508821adc4125cc7fc746d2dd71fe01
FraPiperis <FrancescaPiperis_00@outlook.it>
2025-06-13 16:16:39 +0200

    Spostamento cartelle

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Tickets from './pages/Tickets';
+import ChiSiamo from './pages/ChiSiamo';
+import Eventi from './pages/Eventi';
+import Festival from './pages/Festival';
+import Contatti from './pages/Contatti';
+import Home from './pages/Home'; // Assicurati di avere anche questa pagina
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/ChiSiamo" element={<ChiSiamo />} />
+        <Route path="/Eventi" element={<Eventi />} />
+        <Route path="/Festival" element={<Festival />} />
+        <Route path="/Tickets" element={<Tickets />} />
+        <Route path="/Contatti" element={<Contatti />} />
+      </Routes>
+    </Router>
   );
 }
 
